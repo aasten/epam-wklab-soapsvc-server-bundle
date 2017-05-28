1bdd57b0a58c90e2a04a82747ec08d25390c8697
Stenukhin Alexey <aasten@gmail.com>
2017-05-28 17:45:42 +0300

    making bundle of submodule server

--- src/main/java/com/epam/wklab/soap/simpl/NoMatchedFriendsException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.wklab.soap.simpl;
-
-/**
- * Created by sten on 11.04.17.
- */
-public class NoMatchedFriendsException extends Exception {
-    public NoMatchedFriendsException(String msg) { super(msg); }
-}
